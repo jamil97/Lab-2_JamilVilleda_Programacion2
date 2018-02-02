eb04dc1c8993dd45ddbc384357e06edd20d97467
jamil97 <jamvilleda97@live.com>
2018-02-02 13:41:05 -0600

    Estoy creando las clases

--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -15,7 +15,9 @@ public class JavaApplication15 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bienvenido al sistema");
+        System.out.println("Elija una opcion porfavor: ");
+        System.out.println("a- Agregar detectives \n" + "b- Eliminar detectives \n" + "c- LogIn de detectives \n");
     }
-    
+
 }
