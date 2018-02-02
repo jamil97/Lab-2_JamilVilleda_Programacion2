67e1cbef80912df979e018d2068a47ba7f043660
jamil97 <jamvilleda97@live.com>
2018-02-02 16:13:41 -0600

    Listo con algunas verificaciones

--- JavaApplication15/src/javaapplication15/Detectives.java
+++ JavaApplication15/src/javaapplication15/Detectives.java
@@ -10,5 +10,79 @@ package javaapplication15;
  * @author Jamil
  */
 public class Detectives {
-    
+
+    private String nombre;
+    private int edad;
+    private String lugar;
+    private int labor;
+    private int level;
+    private String user;
+    private String contra;
+
+    public Detectives(String nombre, int edad, String lugar, int labor, int level, String user, String contra) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.lugar = lugar;
+        this.labor = labor;
+        this.level = level;
+        this.user = user;
+        this.contra = contra;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLabor(int labor) {
+        this.labor = labor;
+    }
+
+    public int getLabor() {
+        return labor;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
 }
--- JavaApplication15/src/javaapplication15/JavaApplication15.java
+++ JavaApplication15/src/javaapplication15/JavaApplication15.java
@@ -10,6 +10,7 @@ package javaapplication15;
  * @author Jamil
  */
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class JavaApplication15 {
 
@@ -19,22 +20,79 @@ public class JavaApplication15 {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
-        System.out.println("Bienvenido al sistema");
-        System.out.println("Elija una opcion porfavor: ");
-        System.out.println("a- Agregar detectives \n" + "b- Modificar detectives \n" + "c- Eliminar detectives \n" + "d- LogIn de detectives \n");
-        String opcion = sc.next();
+        String estado = " ";
+        String opcion = " ";
+        ArrayList<Detectives> lista = new ArrayList();
+        ArrayList lista2 = new ArrayList();
+        while (!opcion.equalsIgnoreCase("f")) {
+            opcion = JOptionPane.showInputDialog("Bienvenido al sistema usuario! Porfavor haga una eleccion: \n"
+                    + "1- Agregar un detective al archivo."
+                    + "2- Modificar un detective existente."
+                    + "3- Eliminar un detective del sistema."
+                    + "4- Hacer LogIn."
+                    + "5- Salir del sistema.");
+            if (opcion.equalsIgnoreCase("1")) {
+                String nombre, lugar, user, contra;
+                int edad, labor, level;
+                nombre = JOptionPane.showInputDialog("Ingrese su nombre porfavor: ");
+                edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su edad actual: "));
+                lugar = JOptionPane.showInputDialog("Ingrese su lugar de nacimiento: ");
+                labor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su tiempo laborando"));
+                level = Integer.parseInt(JOptionPane.showInputDialog("Ingrese su nivel de sistema: "));
+                user = (JOptionPane.showInputDialog("Ingrese su nuevo usuario: "));
+                contra = JOptionPane.showInputDialog("Ingrese su nueva contra: ");
+                lista.add(new Detectives(nombre, edad, lugar, labor, level, user, contra));
+            }
+            if (opcion.equalsIgnoreCase("2")) {
+                int parametro = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un espacio a modificar: \n"
+                        + "\n 0- Nombre"
+                        + "\n 1- Edad: "
+                        + "\n 2- Nacionalidad:"
+                        + "\n 3- Tiempo laborando:"
+                        + "\n 4- Nivel administrativo:"
+                        + "\n 5- Usuario: "
+                        + "\n 6- Contra"));
+                if (parametro >= 0 && parametro < lista.size() && lista.get(parametro) instanceof Detectives) {
+                    if (parametro == 0) {
+                        String nombre = JOptionPane.showInputDialog("Nuevo nombre");
+                        ((Detectives) lista.get(parametro)).setNombre(nombre);
+                    }
+                    if (parametro == 1) {
+                        int edad = Integer.parseInt(JOptionPane.showInputDialog("Porfavor ingrese su nueva edad: "));
+                        ((Detectives) lista.get(parametro)).setEdad(edad);
+                    }
+                    if (parametro == 2) {
+                       String nacion = JOptionPane.showInputDialog("Ingrese su nueva nacionalidad porfavor: ");
+                        ((Detectives) lista.get(parametro)).setLugar(nacion);
+                    }
+                    if (parametro == 3) {
+                       int laborando = Integer.parseInt(JOptionPane.showInputDialog("Porfavor ingrese el tiempo trabajando: "));
+                        ((Detectives) lista.get(parametro)).setEdad(laborando);
+                    }
+                    if (parametro == 4) {
+                         int nivel = Integer.parseInt(JOptionPane.showInputDialog("Porfavor ingrese su nivel ejecutivo: "));
+                        ((Detectives) lista.get(parametro)).setEdad(nivel);
+                    }
+                    if (parametro == 5) {
+                       String usuario = JOptionPane.showInputDialog("Ingrese su nuevo username: ");
+                        ((Detectives) lista.get(parametro)).setNombre(usuario);
+                    }
+                    if (parametro == 6) {
+                        String contra = JOptionPane.showInputDialog("Ingrese su nueva contra: ");
+                        ((Detectives) lista.get(parametro)).setNombre(contra);
+                    }
+                }
+            }
+            if (opcion.equalsIgnoreCase("3")) {
 
-        if (opcion.equalsIgnoreCase("a")) {
+            }
+            if (opcion.equalsIgnoreCase("4")) {
 
+            }
+            if (opcion.equalsIgnoreCase("5")) {
+                System.exit(0);
+            }
         }
-        if (opcion.equalsIgnoreCase("b")) {
-
-        }
-        if (opcion.equalsIgnoreCase("c")) {
-
-        }
-
     }
-
+}
 }
